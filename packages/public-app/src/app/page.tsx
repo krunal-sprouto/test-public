48fe2f4a8ba4a84fe75f927c2e348c1bba7f1d7e
Niranjan-Sprouto <niranjan.sprouto@gmail.com>
2025-10-08 16:07:58 +0530

    test initial commit

---
+++ packages/public-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "shared-ui";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-4">Public App</h1>
+      <Button />
+    </main>
+  );
+}
---
+++ packages/shared-ui/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Button = () => {
+  return (
+    
+    <button style={{
+      backgroundColor: 'purple',
+      color: 'white',
+      padding: '10px 20px',
+      border: 'none',
+      borderRadius: '5px'
+    }}>
+      Updated Shared Button
+    </button>
+  );
+};
